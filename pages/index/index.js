e13d29eff0993a73f170799a1429414caa938934
zhangzhulei <zhangzhulei@100tal.com>
2019-12-04 14:07:10 +0800

    首页轮播图实现

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,34 +1,8 @@
-const app = getApp();
-
 Page({
-  onLoad(query) {
-    // 页面加载
-  },
-  onReady() {
-    // 页面加载完成
-    console.log(app.name)
-    console.log(app.course)
-    app.sayHello()
-  },
-  onShow() {
-    // 页面显示
-  },
-  onHide() {
-    // 页面隐藏
-  },
-  onUnload() {
-    // 页面被关闭
-  },
-  onTitleClick() {
-    // 标题被点击
-  },
-  onPullDownRefresh() {
-    // 页面被下拉
-  },
-  onReachBottom() {
-    // 页面被拉到底部
-  },
-  onShareAppMessage() {
-    // 返回自定义分享信息
-  },
+  data: {
+    swiperList: [
+      '/resources/img/1.png',
+      '/resources/img/4.png'
+    ]
+  }
 });
