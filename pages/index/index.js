aaed8d764a3d43381b46f70963e5d198691cf6fd
zhangzhulei <zhangzhulei@100tal.com>
2019-12-05 19:18:24 +0800

    首页动态获取数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,102 @@
 Page({
   data: {
-    swiperList: [
-      '/resources/img/1.png',
-      '/resources/img/4.png'
-    ]
+    swiperList: [],
+    recList: [],
+    newList: []
+  },
+  onLoad () {
+    my.request({
+      url: 'http://www.imoocdsp.com/index/carousels',
+      method: 'post',
+      dataType: 'json',
+      success: (result) => {
+        console.log(result.data.data)
+        this.setData({
+          swiperList: result.data.data
+        })
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+    my.request({
+      url: 'http://www.imoocdsp.com/index/items/rec',
+      method: 'post',
+      data: {
+        listType: 'rec'
+      },
+      dataType: 'json',
+      success: (result) => {
+        console.log(result.data.data)
+        this.setData({
+          recList: result.data.data
+        })
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+    my.request({
+      url: 'http://www.imoocdsp.com/index/items/new',
+      method: 'post',
+      dataType: 'json',
+      success: (result) => {
+        console.log(result.data.data)
+        this.setData({
+          newList: result.data.data
+        })
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+  },
+  async onPullDownRefresh() {
+    await my.request({
+      url: 'http://www.imoocdsp.com/index/carousels',
+      method: 'post',
+      dataType: 'json',
+      success: (result) => {
+        console.log(result.data.data)
+        this.setData({
+          swiperList: result.data.data
+        })
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+    await my.request({
+      url: 'http://www.imoocdsp.com/index/items/rec',
+      method: 'post',
+      data: {
+        listType: 'rec'
+      },
+      dataType: 'json',
+      success: (result) => {
+        console.log(result.data.data)
+        this.setData({
+          recList: result.data.data
+        })
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+    await my.request({
+      url: 'http://www.imoocdsp.com/index/items/new',
+      method: 'post',
+      dataType: 'json',
+      success: (result) => {
+        console.log(result.data.data)
+        this.setData({
+          newList: result.data.data
+        })
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+    my.stopPullDownRefresh();
   }
 });
