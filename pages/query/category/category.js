31b78304a10a7f0fa16b2307b04363554876731b
zhangzhulei <zhangzhulei@100tal.com>
2019-12-07 21:44:19 +0800

    列表分类页面搭建

--- pages/query/category/category.js
+++ pages/query/category/category.js
@@ -1,4 +1,40 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    categoryList: []
+  },
+  onLoad() {
+    my.request({
+      url: 'http://www.imoocdsp.com/cats',
+      method: 'post',
+      dataType: 'json',
+      success: (result) => {
+        this.setData({
+          categoryList: result.data.data
+        })
+      },
+      fail: (err) => {
+        console.log(err)
+      }
+    })
+  },
+  handleSearch (event) {
+    const itemName = event.detail.value
+    if (!itemName && itemName !== 0) {
+      return
+    }
+    my.request({
+      url: 'http://www.imoocdsp.com/items/search',
+      method: 'post',
+      dataType: 'json',
+      data: {
+        itemName: event.detail.value
+      },
+      success: function(result) {
+        console.log(result.data.data)
+      },
+      fail: function(err) {
+
+      }
+    })
+  }
 });
