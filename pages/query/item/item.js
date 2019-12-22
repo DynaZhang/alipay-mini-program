f2379ed7172e45452070b752293177bf9a56b96c
zhangzhulei <13339687922@163.com>
2019-12-22 23:18:02 +0800

    购物车动画实现

--- pages/query/item/item.js
+++ pages/query/item/item.js
@@ -1,6 +1,46 @@
 Page({
   data: {
-    itemDetail: null
+    itemDetail: null,
+    animationOpacity: 0,
+    animationInfo: {},
+    cartIcon: '/resources/icon/smallIco/cart-empty.png'
+  },
+  onShow() {
+    let animation = my.createAnimation();
+    this.setData({
+      animationInfo: animation.export()
+    })
+  },
+  addToCart() {
+    this.setData({
+      animationOpacity: 1
+    })
+    this.showAddToCartAnimation()
+  },
+  showAddToCartAnimation() {
+    let animation = my.createAnimation({
+      duration: 500
+    }); 
+    this.animation = animation
+    this.animation.rotate(-180).translateX("296rpx").step()
+    this.setData({
+      animationInfo: this.animation.export(),
+      cartIcon: '/resources/icon/smallIco/cart-full.png'
+    })
+
+    setTimeout(() => {
+      this.setData({
+        animationOpacity: 0
+      })
+      let animation = my.createAnimation({
+          duration: 500
+        }); 
+        this.animation = animation
+        this.animation.rotate(0).translateX(0).step()
+        this.setData({
+          animationInfo: this.animation.export()
+        })
+    }, 500)
   },
   onLoad(params) {
     const itemId = params.id
