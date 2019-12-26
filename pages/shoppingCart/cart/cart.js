8e9164a354f4d3ac599161fbeb1bd94256a2d488
zhangzhulei <zhangzhulei@100tal.com>
2019-12-26 19:48:42 +0800

    购物车交互实现

--- pages/shoppingCart/cart/cart.js
+++ pages/shoppingCart/cart/cart.js
@@ -3,7 +3,8 @@ Page({
     cartItemList: [],
     emptyHidden: false,
     totalPrice: 0,
-    checkAll: false
+    checkAll: true,
+    selectedItems: []
   },
   onShow() {
     const cartItemIdArray = my.getStorageSync({
@@ -37,7 +38,7 @@ Page({
       headers: {'content-type': 'application/x-www-form-urlencoded'},
       success: (result) => {
         const itemList = result.data.data
-        let totalPrice = 0
+        const selectedItems = []
         for (let i = 0; i < itemList.length; i++) {
           const cartItem = getApp().findCartItemById(cartItemIdArray, itemList[i].id) 
           if (cartItem === null) {
@@ -45,14 +46,18 @@ Page({
           } else {
             itemList[i].counts = cartItem.counts
             itemList[i].isSelected = cartItem.isSelected
-            totalPrice += itemList[i].priceDiscountYuan * cartItem.counts
+            const symbol = `${itemList[i].id};${itemList[i].priceDiscountYuan};${itemList[i].counts}`
+            itemList[i].symbol = symbol
+            selectedItems.push(symbol)
           }
         }
-
+        console.log(selectedItems)
         this.setData({
           cartItemList: itemList.filter((item,index) => {return item.counts > 0}),
-          totalPrice
+          selectedItems
         })
+        this.calPrice()
+        console.log(this.data.selectedItems)
         my.hideLoading()
       },
       error: (error) => {
@@ -66,17 +71,48 @@ Page({
       url: 'pages/index/index'
     });
   },
-  handleItemChange(event) {
-    const selectedValue = event.detail.value
+  handleSelectItem(event) {
+    const selectedValue = event.target.dataset.itemSymbol
     console.log(selectedValue)
-    if (selectedValue.length === this.data.cartItemList.length) {
+    const pos = this.data.selectedItems.indexOf(selectedValue)
+    if (pos === -1) {
       this.setData({
-        checkAll: true
+        selectedItems: [...this.data.selectedItems,selectedValue]
       })
     } else {
+      const selectedItems = this.data.selectedItems
+      selectedItems.splice(pos,1)
       this.setData({
-        checkAll: false
+        selectedItems
       })
     }
+    this.calPrice()
+    this.setData({
+      checkAll: this.data.selectedItems.length === this.data.cartItemList.length
+    })
+  },
+  handleSelectAll() {
+    const selectedItems = []
+    if (!this.data.checkAll) {
+      this.data.cartItemList.forEach(item => {
+        selectedItems.push(item.id)
+      })
+    }
+    this.setData({
+      selectedItems
+    })
+    this.calPrice()
+  },
+  calPrice() {
+    let totalPrice = 0
+    this.data.selectedItems.forEach(value => {
+      const itemDetail = value.split(';')
+      const priceDiscountYuan = parseFloat(itemDetail[1])
+      const counts = parseInt(itemDetail[2])
+      totalPrice += priceDiscountYuan * counts
+    })
+    this.setData({
+      totalPrice
+    })
   }
 });
